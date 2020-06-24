6238284f90f284ddab6e160d9bf47276afff5c4d
PranayBandaru <pranay18539@mechyd.ac.in>
2020-06-24 15:52:39 +0530

    Test

--- app/src/main/java/com/example/dtlapp/activity2.java
+++ app/src/main/java/com/example/dtlapp/activity2.java
@@ -110,7 +110,6 @@ public class activity2 extends AppCompatActivity {
         setSupportActionBar(toolbar);
         mprog = new ProgressDialog(this);
         //Firebase.setAndroidContext(this);
-
         //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         mstorage = FirebaseStorage.getInstance().getReference("Photos");
         mdatabse = FirebaseDatabase.getInstance().getReference("Photos");
@@ -153,6 +152,7 @@ public class activity2 extends AppCompatActivity {
 
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            ActivityCompat.requestPermissions(activity2.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
             ActivityCompat.requestPermissions(activity2.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
 
 
@@ -235,6 +235,222 @@ public class activity2 extends AppCompatActivity {
             imageView.setImageBitmap(photo);
         }
     }*/
+    public class GPSTracker extends Service implements LocationListener {
+
+
+        //private static final Location TODO = ;
+        private final Context mContext;
+
+        // Flag for GPS status
+        boolean isGPSEnabled = false;
+
+        // Flag for network status
+        boolean isNetworkEnabled = false;
+
+        // Flag for GPS status
+        boolean canGetLocation = false;
+
+        Location location; // Location
+        public double latitude; // Latitude
+        public double longitude; // Longitude
+
+        // The minimum distance to change Updates in meters
+        private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+        // The minimum time between updates in milliseconds
+        private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+        // Declaring a Location Manager
+        protected LocationManager locationManager;
+
+        public GPSTracker(Context context) {
+            this.mContext = context;
+            getLocation();
+        }
+
+        public Location getLocation() {
+            try {
+                locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+               /* if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return TODO;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);*/
+                // Getting GPS status
+                isGPSEnabled = locationManager
+                        .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                // Getting network status
+                isNetworkEnabled = locationManager
+                        .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+                if (!isGPSEnabled && !isNetworkEnabled) {
+                    // No network provider is enabled
+                } else {
+                    this.canGetLocation = true;
+                    if (isNetworkEnabled) {
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+
+                        }
+                        locationManager.requestLocationUpdates(
+                                LocationManager.NETWORK_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("Network", "Network");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                    // If GPS enabled, get latitude/longitude using GPS Services
+                    if (isGPSEnabled) {
+                        if (location == null) {
+                            locationManager.requestLocationUpdates(
+                                    LocationManager.GPS_PROVIDER,
+                                    MIN_TIME_BW_UPDATES,
+                                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                            Log.d("GPS Enabled", "GPS Enabled");
+                            if (locationManager != null) {
+                                location = locationManager
+                                        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                if (location != null) {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return location;
+        }
+
+
+        /**
+         * Stop using GPS listener
+         * Calling this function will stop using GPS in your app.
+         * */
+        public void stopUsingGPS(){
+            if(locationManager != null){
+                locationManager.removeUpdates(GPSTracker.this);
+            }
+        }
+
+
+        /**
+         * Function to get latitude
+         * */
+        public double getLatitude(){
+            if(location != null){
+                latitude = location.getLatitude();
+            }
+
+            // return latitude
+            return latitude;
+        }
+
+
+        /**
+         * Function to get longitude
+         * */
+        public double getLongitude(){
+            if(location != null){
+                longitude = location.getLongitude();
+            }
+
+            // return longitude
+            return longitude;
+        }
+
+        /**
+         * Function to check GPS/Wi-Fi enabled
+         * @return boolean
+         * */
+        public boolean canGetLocation() {
+            return this.canGetLocation;
+        }
+
+
+        /**
+         * Function to show settings alert dialog.
+         * On pressing the Settings button it will launch Settings Options.
+         * */
+        public void showSettingsAlert(){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+            // Setting Dialog Title
+            alertDialog.setTitle("GPS is settings");
+
+            // Setting Dialog Message
+            alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+            // On pressing the Settings button.
+            alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    mContext.startActivity(intent);
+                }
+            });
+
+            // On pressing the cancel button
+            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            // Showing Alert Message
+            alertDialog.show();
+        }
+
+
+        @Override
+        public void onLocationChanged(Location location) {
+        }
+
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+
+        @Override
+        public IBinder onBind(Intent arg0) {
+            return null;
+        }
+    }
+    GPSTracker gps;
 
 
 
@@ -262,10 +478,13 @@ public class activity2 extends AppCompatActivity {
 
 
         if (requestCode == CAMERA_REQUEST) {
+            gps = new GPSTracker(this);
 
             if (resultCode == RESULT_OK) {
                 //previewCapturedImage();
-                if (lat != 0 && longi != 0) {
+                if (gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
                     // \n is for new line
 
                     cords.setText("geo:" + lat + "," + longi);
